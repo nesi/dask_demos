b6fff981c131e28bd4a4a8ed1084c209a917a2e7
Maxime RIO <maxime.rio@niwa.co.nz>
2020-07-28 18:58:54 +0000

    add timing information in the notebook script

--- src/hyperparameters_search_basic.py
+++ src/hyperparameters_search_basic.py
@@ -5,6 +5,7 @@
 
 # +
 import pprint
+import time
 
 import numpy as np
 from sklearn.datasets import fetch_openml
@@ -20,8 +21,8 @@ from dask_jobqueue import SLURMCluster
 
 # Load MNIST data from [OpenML](https://www.openml.org/d/554).
 
-X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
-X = X / 255.
+X, y = fetch_openml("mnist_784", version=1, return_X_y=True)
+X = X / 255.0
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, stratify=y, train_size=10000, test_size=10000, random_state=42
@@ -29,7 +30,10 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # Fit a simple multi-layer perceptron neural net.
 
+start = time.perf_counter()
 mlp = MLPClassifier().fit(X_train, y_train)
+elapsed = time.perf_counter() - start
+print(f"Model fitting took {elapsed:0.2f}s.")
 
 y_pred = mlp.predict(X_test)
 mlp_acc = accuracy_score(y_test, y_pred)
@@ -38,9 +42,9 @@ print(f"Baseline MLP test accuracy is {mlp_acc * 100:.2f}%.")
 # Tune hyper-parameters using a random search strategy.
 
 param_grid = {
-    "hidden_layer_sizes": [(10,), (50,), (100,)],
+    "hidden_layer_sizes": [(50,), (100,), (200,)],
     "alpha": np.logspace(-5, -3, 3),
-    "learning_rate_init": np.logspace(-4, -2, 3)
+    "learning_rate_init": np.logspace(-4, -2, 3),
 }
 mlp_tuned = GridSearchCV(MLPClassifier(), param_grid, verbose=1)
 
@@ -56,7 +60,12 @@ client = Client(cluster)
 # scheduled and distributed on nodes of the HPC.
 
 with joblib.parallel_backend("dask", wait_for_workers_timeout=600):
+    start = time.perf_counter()
     mlp_tuned.fit(X_train, y_train)
+    elapsed = time.perf_counter() - start
+
+n_jobs = len(mlp_tuned.cv_results_["params"]) * mlp_tuned.n_splits_
+print(f"Model fitting took {elapsed:0.2f}s ({elapsed / n_jobs:0.2f}s per model fit).")
 
 # Enjoy an optimized model :).
 
@@ -64,5 +73,4 @@ y_pred_tuned = mlp_tuned.predict(X_test)
 mlp_tuned_acc = accuracy_score(y_test, y_pred_tuned)
 print(f"Tuned MLP test accuracy is {mlp_tuned_acc * 100:.2f}%.")
 
-print(f"Best hyper-parameters found:")
-pprint.pp(mlp_tuned.best_params_)
+print(f"Best hyper-parameters: {mlp_tuned.best_params_}")
